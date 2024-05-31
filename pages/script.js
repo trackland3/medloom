c76caf0953f2ff4edc10ff2d581d2c9ec1bead59
Didera <147242984+Didera@users.noreply.github.com>
2024-05-31 16:32:10 +0530

    not done in the operations

--- pages/script.js
+++ pages/script.js
@@ -1,14 +1,33 @@
+//preloader --------------------------------------------------------------------
+
+const preloader = document.getElementById("preloader");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
+
+//preloader --------------------------------------------------------------------
+
+
+
 //navbar color change ----------------------------------------------------------
 
 const heighty = window.innerHeight
 console.log(heighty);
 window.addEventListener('scroll', (e) => {
     const scrolly = window.scrollY
-    if (scrolly > 200) {
-        document.getElementById("navbar").style.backgroundColor = '#ffffff'
+    if (scrolly > 100) {
+        document.getElementById("navb").style.backgroundColor = '#ffffff'
     } else {
-        document.getElementById("navbar").style.backgroundColor = '#85bce100'
+        document.getElementById("navb").style.backgroundColor = '#85bce100'
     }
+    if (scrolly > 100) {
+        document.getElementById("navba").style.backgroundColor = '#ffffff'
+    } else {
+        document.getElementById("navba").style.backgroundColor = '#85bce100'
+    }
+    
 })
 
 //navbar color change ----------------------------------------------------------
@@ -21,19 +40,23 @@ document.getElementById("logo").addEventListener("click", () => {
     navbox.classList.toggle('active')
     if (navbox.style.right === "-200px") {
     navbox.style.right = '0px'
+    document.getElementById("cont").style.filter = 'blur(5px)';
     console.log('hello');
   } else {
     navbox.style.right = '-200px'
+    document.getElementById("cont").style.filter = 'blur(0px)';
     console.log('helloo');
+
   }
 
+    
 });
 
 
 //responsive navbar ------------------------------------------------------------
 
 
-
+//scroll --
 
 //horizontal scrolling ---------------------------------------------------------
 
@@ -61,3 +84,22 @@ function transform(section) {
 }
 
 //horizontal scrolling ---------------------------------------------------------
+
+//scroll reveal ----------------------------------------------------------------
+
+const scrollTriggers = document.querySelectorAll("[scrollTrigger]");
+
+const scrollTrigger = function () {
+  for (let i = 0, len = scrollTriggers.length; i < len; i++) {
+    if (scrollTriggers[i].getBoundingClientRect().top < window.innerHeight / 1.15) {
+      scrollTriggers[i].classList.add("trigger");
+    } else {
+      scrollTriggers[i].classList.remove("trigger");
+    }
+  }
+}
+
+window.addEventListener("scroll", scrollTrigger);
+
+window.addEventListener("load", scrollTrigger);
+
