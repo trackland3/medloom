b12b8ad86a54382ddc5c1323d0afa5396abbf975
useriskavindu <147843787+useriskavindu@users.noreply.github.com>
2024-06-08 00:33:21 +0530

    Updated on  6/8/2024

--- script.js
+++ script.js
@@ -1,18 +1,63 @@
+
+//preloader --------------------------------------------------------------------
+
+const preloader = document.getElementById("preloader");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
+
+//preloader --------------------------------------------------------------------
+
+
+
 //navbar color change ----------------------------------------------------------
 
 const heighty = window.innerHeight
 console.log(heighty);
 window.addEventListener('scroll', (e) => {
     const scrolly = window.scrollY
-    if (scrolly > 800) {
-        document.querySelector('.navbar').style.backgroundColor = '#427191'
+    if (scrolly > 100) {
+        document.getElementById("navb").style.backgroundColor = '#ffffff'
     } else {
-        document.querySelector('.navbar').style.backgroundColor = '#85bce1'
+        document.getElementById("navb").style.backgroundColor = '#85bce100'
     }
+    if (scrolly > 100) {
+        document.getElementById("navba").style.backgroundColor = '#ffffff'
+    } else {
+        document.getElementById("navba").style.backgroundColor = '#85bce100'
+    }
+    
 })
 
+//navbar color change ----------------------------------------------------------
+
+//responsive navbar ------------------------------------------------------------
+
+const navbox = document.getElementById("navbox") 
+
+document.getElementById("logo").addEventListener("click", () => {
+    navbox.classList.toggle('active')
+    if (navbox.style.right === "-300px") {
+    navbox.style.right = '0px'
+    document.getElementById("cont").style.filter = 'blur(5px)';
+    console.log('hello');
+  } else {
+    navbox.style.right = '-300px'
+    document.getElementById("cont").style.filter = 'blur(0px)';
+    console.log('helloo');
+
+  }
+
+    
+});
 
 
+//responsive navbar ------------------------------------------------------------
+
+
+//scroll --
 
 //horizontal scrolling ---------------------------------------------------------
 
@@ -34,9 +79,30 @@ function transform(section) {
     let percentage = ((window.scrollY - offsetTop) / window.innerHeight) * 100;
     // console.log(window.scrollY);
     // console.log(percentage);
-    percentage = percentage < 0 ? 0 : percentage > 200 ? 200 : percentage; 
+    percentage = percentage < 0 ? 0 : percentage > 100 ? 100 : percentage; 
     scrollsect.style.transform = `translate3d(${-(percentage)}vw, 0, 0)`
 
 }
 
 //horizontal scrolling ---------------------------------------------------------
+
+//scroll reveal ----------------------------------------------------------------
+
+const scrollTriggers = document.querySelectorAll("[scrollTrigger]");
+
+const scrollTrigger = function () {
+  for (let i = 0, len = scrollTriggers.length; i < len; i++) {
+    if (scrollTriggers[i].getBoundingClientRect().top < window.innerHeight / 1.15) {
+      scrollTriggers[i].classList.add("trigger");
+    } else {
+      scrollTriggers[i].classList.remove("trigger");
+    }
+  }
+}
+
+window.addEventListener("scroll", scrollTrigger);
+
+window.addEventListener("load", scrollTrigger);
+
+
+
